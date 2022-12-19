38bfc9fdc685e052a917905c4e5d17ddb280e335
Dpouris <jimpouris0@gmail.com>
2022-12-20 01:59:18 +0200

    add dynamic routes

--- examples/example.go
+++ examples/example.go
@@ -8,70 +8,74 @@ import (
 	Gottp "github.com/dpouris/gottp-client"
 )
 
-type JSONResponse struct {
-	Hey string `json:"hey"`
-	You string `json:"you"`
-}
-
 func main() {
 	g := Gottp.Server()
 
-	g.AddGlobalMiddleware(func(r Gottp.Res, req *Gottp.Req) error {
+	g.AddGlobalMiddleware(func(ctx *Gottp.Ctx) error {
 		fmt.Println("middleware")
 		return nil
 	})
 
-	g.Get("/path", func(r Gottp.Res, req *Gottp.Req) error {
+	g.Get("path/", func(ctx *Gottp.Ctx) error {
 
-		res := JSONResponse{
+		res := struct {
+			Hey string `json:"hey"`
+			You string `json:"you"`
+		}{
 			Hey: "Hello",
 			You: "World",
 		}
 
-		r.NewHeaders(map[string]string{
+		ctx.ResponseWriter.NewHeaders(map[string]string{
 			"Content-Type": "application/json",
 		}, 200)
-		r.JSON(res)
+		ctx.ResponseWriter.JSON(res)
 
 		return nil
 	})
 
-	g.Post("/path", func(r Gottp.Res, req *Gottp.Req) error {
-		db := make([]byte, req.ContentLength)
-		req.Body.Read(db)
+	g.Get("/path/:name", func(ctx *Gottp.Ctx) error {
+		name := ctx.Params.Get("name")
+		ctx.ResponseWriter.Write([]byte(fmt.Sprintf("Hi, my name is %s", name)))
+		return nil
+	})
+
+	g.Post("/path", func(ctx *Gottp.Ctx) error {
+		db := make([]byte, ctx.Request.ContentLength)
+		ctx.Request.Body.Read(db)
 		err := ioutil.WriteFile("./examples/fake_db.txt", db, 0666)
 
 		if err != nil {
 			return err
 		}
 
-		r.NewHeaders(map[string]string{
+		ctx.ResponseWriter.NewHeaders(map[string]string{
 			"Content-Type": "application/json",
 		}, http.StatusCreated)
 
-		r.JSON(db)
+		ctx.ResponseWriter.JSON(db)
 
 		return nil
 	})
 
-	g.Get("/hey", func(r Gottp.Res, req *Gottp.Req) error {
+	g.Get("/hey", func(ctx *Gottp.Ctx) error {
 		heyPage, err := ioutil.ReadFile("./examples/hey.html")
 
 		if err != nil {
 			fmt.Println(err)
 		}
-		r.Write(heyPage)
+		ctx.ResponseWriter.Write(heyPage)
 		return nil
 	})
 
-	g.Get("/logs", func(r Gottp.Res, req *Gottp.Req) error {
+	g.Get("/logs", func(ctx *Gottp.Ctx) error {
 		log_map := make(map[int]any, len(g.Logs))
 
 		for i, v := range g.Logs {
 			log_map[i] = v
 		}
 
-		err := r.JSON(log_map)
+		err := ctx.ResponseWriter.JSON(log_map)
 
 		if err != nil {
 			Gottp.LogError(err.Error(), g.Logger)
